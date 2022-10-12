62cafe4836c34283f6673e38b9bac971bc20718a
Andrew Greenlaw <a.greenlaw8@gmail.com>
2022-10-12 14:46:13 -0600

    Home Page stuff

--- lego-traders.client/src/pages/HomePage.vue
+++ lego-traders.client/src/pages/HomePage.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="container-fluid home">
+  <div class="container-fluid home bg-primary">
+    <div class="row justify-content-center">
+      <div class="col-10 my-5 text-center bg-light p-5">
+        <img src="../assets/img/welcome banner.png" alt="welcome banner">
+      </div>
+    </div>
     <div class="row">
-      <div class="col-12">
+      <div class="col-3 text-center">
+        <button>MARKETPLACE</button>
+      </div>
+      <div class="col-3 text-center">
+        <button>FORUM</button>
+      </div>
+      <div class="col-3 text-center">
+        <button>CREATE YOUR OWN</button>
+      </div>
+      <div class="col-3 text-center">
         <router-link :to="{ name: 'Collection' }">
-          <button>Collection Page</button>
+          <button>COLLECTIONS</button>
         </router-link>
       </div>
     </div>
   </div>
+  <!-- <router-link :to="{ name: 'Collection' }">
+    <button>Collection Page</button>
+  </router-link> -->
 </template>
 
 <script>
@@ -19,10 +36,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  background-image: url('blue-lego.webp');
-  background-size: cover;
-  background-position: top left;
-  height: 100vh;
-}
+// .home {
+//   background-image: url('blue-lego.webp');
+//   background-size: cover;
+//   background-position: top left;
+//   height: 100vh;
+// }
 </style>
