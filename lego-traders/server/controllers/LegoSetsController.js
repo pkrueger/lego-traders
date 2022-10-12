d68ab0e1ed88e03f94a2934a2f7138643137c011
Coel Allen <coelallen@gmail.com>
2022-10-12 14:30:08 -0600

    backend update

--- lego-traders/server/controllers/LegoSetsController.js
+++ lego-traders/server/controllers/LegoSetsController.js
@@ -6,6 +6,35 @@ export class LegoSetsController extends BaseController{
     super('api/sets')
     this.router
     .get('', this.getAllSets)
+    .get('/tradable', this.getTradableSets)
+    .get('/:legoSetId', this.getSetBySetId)
+    .get('/:id/sets', this.getSetsByProfileId)
+    
+
+  }
+    async getSetsByProfileId(req, res, next) {
+    try {
+      const sets = await legoSetsService.getSetsByProfileId(req.param.id)
+      res.send(sets)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async getSetBySetId(req, res, next) {
+try {
+  const set = await legoSetsService.getSetBySetId(req.params.legoSetId)
+  res.send(set)
+} catch (error) {
+  next(error)
+}
+  }
+  async getTradableSets(req, res, next) {
+    try {
+      const tradableSets = await legoSetsService.getTradableSets()
+      res.send(tradableSets)
+    } catch (error) {
+      next(error)
+    }
   }
   async getAllSets(req, res, next) {
 try {
