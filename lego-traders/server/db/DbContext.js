cfa20670a7f3199433580dd0da0578f199555a2d
EricTimRussell <erictim.russell@live.com>
2022-10-12 16:41:33 -0600

    made changes to legoSetService

--- lego-traders/server/db/DbContext.js
+++ lego-traders/server/db/DbContext.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { AccountSchema } from "../models/Account";
+import { CommentSchema } from "../models/Comment.js";
 import { LegoSetSchema } from "../models/LegoSet.js";
 import { PostSchema } from "../models/Post.js";
 
@@ -9,6 +10,7 @@ class DbContext {
   LegoSets = mongoose.model('LegoSet', LegoSetSchema)
 
   Posts = mongoose.model('Post', PostSchema)
+  Comments = mongoose.model('Comment', CommentSchema)
 }
 
 export const dbContext = new DbContext();
