f00c087ee894b16063772fafb42b3147022b1c51
Andrew Greenlaw <a.greenlaw8@gmail.com>
2022-10-13 11:21:03 -0600

    small changes to the comments

--- lego-traders/server/controllers/CommentsController.js
+++ lego-traders/server/controllers/CommentsController.js
@@ -6,33 +6,35 @@ export class CommentsController extends BaseController {
   constructor() {
     super('api/comments')
     this.router
-    .get('/forum/:postId', this.getCommentsByPostId)
-    .get('/sets/:set_num', this.getCommentsBySetNum)
-    .get('/:commentId', this.getCommentById)
-    .use(Auth0Provider.getAuthorizedUserInfo)
-    .post('/forum/', this.createForumComment)
-    .delete('/forum/:commentId', this.deleteForumComment)
-    .post('/sets/', this.createSetComment)
-    .delete('/sets/:commentId', this.deleteSetComment)
+      .get('/forum/:postId', this.getCommentsByPostId)
+      .get('/sets/:set_num', this.getCommentsBySetNum)
+      .get('/:commentId', this.getCommentById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('/forum/', this.createForumComment)
+      .delete('/forum/:commentId', this.deleteForumComment)
+      .post('/sets/', this.createSetComment)
+      .delete('/sets/:commentId', this.deleteSetComment)
   }
   async deleteSetComment(req, res, next) {
     try {
       const comment = await commentsService.deleteComment(req.params.commentId)
       res.send(comment)
-      
+
     } catch (error) {
       next(error)
     }
   }
   async getCommentById(req, res, next) {
-try {
-  const comment = await commentsService.getCommentById(req.params.commentId)
-  res.send(comment)
-} catch (error) {
-  next(error)
-}  }
+    try {
+      const comment = await commentsService.getCommentById(req.params.commentId)
+      res.send(comment)
+    } catch (error) {
+      next(error)
+    }
+  }
   async createSetComment(req, res, next) {
     try {
+      req.body.creatorId = req.userInfo.id
       const comment = await commentsService.createComment(req.body)
       res.send(comment)
     } catch (error) {
@@ -46,7 +48,7 @@ try {
     } catch (error) {
       next(error)
     }
-    
+
   }
   async createForumComment(req, res, next) {
     try {
--- lego-traders/server/services/CommentsService.js
+++ lego-traders/server/services/CommentsService.js
@@ -4,29 +4,34 @@ import { BadRequest, Forbidden } from "../utils/Errors.js";
 class CommentsService {
   async deleteComment(commentId, userInfo) {
     const comment = await this.getCommentById(commentId)
-    // NOTE again with the red squiggles!
-     if (comment.creatorId != userInfo) {
+    if (comment.creatorId != userInfo) {
       throw new Forbidden("This is not your Post.");
     }
-    await dbContext.Comments.findByIdAndDelete(commentId)
+    // await dbContext.Comments.findByIdAndDelete(commentId)
+    // NOTE what is the difference between remove and delete?
+    comment.delete()
     return comment
   }
   async createComment(commentData) {
     const comment = await dbContext.Comments.create(commentData)
+    await comment.populate('creator', 'name picture')
     return comment
   }
   async getCommentsBySetNum(set_num) {
-    const comments = await dbContext.Comments.find({set_num})
+    const comments = await dbContext.Comments.find({ set_num })
     return comments
   }
   async getCommentsByPostId(postId) {
-    const comments = await dbContext.Comments.find({postId})
+    const comments = await dbContext.Comments.find({ postId })
+    if (!comments) {
+      throw new BadRequest('Invalid or Bad PostId')
+    }
     return comments
   }
 
-  async getCommentById(commentId){
+  async getCommentById(commentId) {
     const comment = await dbContext.Comments.findById(commentId)
-    if(!comment){
+    if (!comment) {
       throw new BadRequest('Bad Comment Id')
     }
     return comment
