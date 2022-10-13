eb19d623628fd6e7252776d31f636ff5b6cb0d2f
Randyhall91 <randyrocks11@gmail.com>
2022-10-13 12:40:37 -0600

    lunch

--- lego-traders.client/src/components/LegoSetCard.vue
+++ lego-traders.client/src/components/LegoSetCard.vue
@@ -1,28 +1,28 @@
 <template>
   <!-- When Adding a Set Card it needs to be inside a ROW for proper formating -->
-  <div class="">
-    <div class="card card-size">
-      <img :src="legoSet.set_img_url" class="img-fluid p-2 img-size" alt="">
-      <div class="card-body p-2">
-        <h5>{{legoSet.name}}</h5>
-        <p class="m-0">Set ID: {{legoSet.set_num}}</p>
-        <p class="m-0">Year: {{legoSet.year}}</p>
-        <!-- <p class="m-0">Number of Parts: {{legoSet.num_parts}}</p> -->
 
-        <div v-if="account.id == legoSet.ownerId && account.id" class="form-check">
-          <input class="form-check-input" :checked="legoSet.toggleisUpForTrade" type="checkbox" id="isUpForTrade"
-            @change="toggleisUpForTrade()">
-          <label class="form-check-label" for="flexCheckDefault">
-            Check is this set is up for trade
-          </label>
-        </div>
-        <div>
-          <button class="btn btn-primary" v-if="!legoSet.ownerId" @click="addSetToAccount(legoSet)">Add to
-            Account</button>
-        </div>
+  <div class="card card-size">
+    <img :src="legoSet.set_img_url" class="img-fluid p-2 img-size" alt="">
+    <div class="card-body p-2">
+      <h5>{{legoSet.name}}</h5>
+      <p class="m-0">Set ID: {{legoSet.set_num}}</p>
+      <p class="m-0">Year: {{legoSet.year}}</p>
+      <!-- <p class="m-0">Number of Parts: {{legoSet.num_parts}}</p> -->
+
+      <div v-if="account.id == legoSet.ownerId && account.id" class="form-check">
+        <input class="form-check-input" :checked="legoSet.toggleisUpForTrade" type="checkbox" id="isUpForTrade"
+          @change="toggleisUpForTrade()">
+        <label class="form-check-label" for="flexCheckDefault">
+          Check is this set is up for trade
+        </label>
+      </div>
+      <div>
+        <button class="btn btn-primary" v-if="!legoSet.ownerId" @click="addSetToAccount(legoSet)">Add to
+          Account</button>
       </div>
     </div>
   </div>
+
 </template>
 
 
