e8b001fd6ff4dc1605565b0422192988d79cfe60
Randyhall91 <randyrocks11@gmail.com>
2022-10-13 16:08:05 -0600

    profile

--- lego-traders.client/src/pages/ProfilePage.vue
+++ lego-traders.client/src/pages/ProfilePage.vue
@@ -11,7 +11,37 @@
           <p>{{account.desc}}</p>
         </div>
       </div>
+      <div class="col-md-1">
+        <div>
+          <button type="button" class="btn btn-primary">Message User</button>
+        </div>
+      </div>
 
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h3>Owned Sets:</h3>
+        <div class="row">
+          <!-- RouterLink to Set Details page -->
+          <div class="col-3" v-for="l in ownedLegoSets">
+
+            <LegoSetCard :key="l.id" :legoSet="l" />
+
+          </div>
+          <!-- TODO Component for My Sets  -->
+        </div>
+        <div class="col-12">
+          <h3>WishList:</h3>
+          <div class="row">
+            <div class="col-3" v-for="l in wishListLegoSets">
+
+              <LegoSetCard :key="l.id" :legoSet="l" />
+
+            </div>
+          </div>
+          <!-- TODO Component for My Wishlist  v-if="!l.isOwned"  -->
+        </div>
+      </div>
     </div>
   </div>
 
