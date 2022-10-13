d7c0a662b6dbbc9454ebb3632a53484f77e66056
pkrueger <sendthistopatrick@gmail.com>
2022-10-13 10:58:31 -0600

    Add support for num_parts from api

--- lego-traders.client/src/models/LegoSet.js
+++ lego-traders.client/src/models/LegoSet.js
@@ -1,13 +1,14 @@
 export class LegoSet {
   constructor(data) {
-    this.id = data.id
-    this.ownerId = data.ownerId
-    this.set_num = data.set_num
-    this.name = data.name
-    this.year = data.year
-    this.theme_id = data.theme_id
-    this.set_img_url = data.set_img_url
-    this.isOwned = data.isOwned
-    this.isUpForTrade = data.isUpForTrade
+    this.id = data.id;
+    this.ownerId = data.ownerId;
+    this.set_num = data.set_num;
+    this.num_parts = data.num_parts;
+    this.name = data.name;
+    this.year = data.year;
+    this.theme_id = data.theme_id;
+    this.set_img_url = data.set_img_url;
+    this.isOwned = data.isOwned;
+    this.isUpForTrade = data.isUpForTrade;
   }
 }
