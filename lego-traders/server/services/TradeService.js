1dbc7debc63409e263e7559d9dc0f499db2e621b
Randyhall91 <randyrocks11@gmail.com>
2022-10-14 14:58:36 -0600

    Trade stuff

--- lego-traders/server/services/TradeService.js
+++ lego-traders/server/services/TradeService.js
@@ -1,10 +1,28 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 
 
 class TradeService {
-  tradeResponse(id) {
+  async tradeResponse(id, status) {
+    const trade = await dbContext.TradeRequest.findByIdAndUpdate(id, { status })
+    if (!trade) { throw new BadRequest('Bad Trade ID') }
 
+    if (trade.status == 'accepted') {
+      const offered = await dbContext.LegoSets.findById(trade.offeredSetId)
+      if (!offered) { throw new BadRequest('Bad LegoSet ID') }
+      const requested = await dbContext.LegoSets.findById(trade.requestedSetId)
+      if (!requested) { throw new BadRequest('Bad LegoSet ID') }
+
+
+      offered.ownerId = trade.requestedAccountId
+      requested.ownerId = trade.ownerId
+
+      offered.save()
+      requested.save()
+    }
+    trade.save()
+    return trade
   }
   async makeTradeRequest(formData) {
     const trade = await dbContext.TradeRequest.create(formData)
