65dbad634287d2a3be35e85f54c2e51f9af553bb
EricTimRussell <erictim.russell@live.com>
2022-10-14 11:16:15 -0600

    scroll to top on page change

--- lego-traders.client/src/pages/CollectionPage.vue
+++ lego-traders.client/src/pages/CollectionPage.vue
@@ -87,6 +87,7 @@ export default {
       async goPage(url) {
         try {
           await legoSetsService.goPage(url)
+          window.scrollTo(0, 500)
         } catch (error) {
           Pop.error(error)
         }
