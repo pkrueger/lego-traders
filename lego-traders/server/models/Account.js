775e7b48cb6c30fccb36127a8380f6343ed1fdf3
EricTimRussell <erictim.russell@live.com>
2022-10-14 12:28:44 -0600

    default image

--- lego-traders/server/models/Account.js
+++ lego-traders/server/models/Account.js
@@ -6,7 +6,7 @@ export const AccountSchema = new Schema(
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String },
+    picture: { type: String, default: "https://www.clonebricks.com/wp-content/uploads/2017/03/01-lego-classic-town-minifigure.jpeg" },
     desc: { type: String, maxlength: 1000 },
     userTheme: { type: String }
     // NOTE If you wish to add additional properties do so here
