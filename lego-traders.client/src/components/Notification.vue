0dfdfbbcde5f2cc224eec013cad70e28eb0512f7
pkrueger <sendthistopatrick@gmail.com>
2022-10-18 14:53:56 -0600

    Notification stuff

--- lego-traders.client/src/components/Notification.vue
+++ lego-traders.client/src/components/Notification.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="component"></div>
+  <div class="component">hello</div>
 </template>
 
 <script>
 export default {
+  props: { notification: { type: Object, required: true } },
   setup() {
     return {};
   },
