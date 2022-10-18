bea429416e0d52b7ff499cd57aabac8beb7a62b2
Coel Allen <coelallen@gmail.com>
2022-10-18 16:20:57 -0600

    chat kinda works

--- lego-traders.client/src/components/ChatComment.vue
+++ lego-traders.client/src/components/ChatComment.vue
@@ -1,22 +1,8 @@
 <template>
-  <div class="d-flex p-2 align-items-center">
-    <div>
-      <img class="profile-picture" :src="comment.creator?.picture" alt="Im the profile picture"
-        :title="comment.creator?.name">
-    </div>
-    <div class="comment-text ms-2 bg-info d-flex flex-grow-1 p-3 justify-content-between">
-      <div>
-        <h5>{{comment.creator?.name}}</h5>
-        <p>{{comment.body}}</p>
-      </div>
-      <div>
-        <button @click="removeComment()" class="btn selectable" aria-label="Delete this Comment?"
-          v-if="comment.creatorId == account.id">
-          <i class="mdi mdi-close" title="delete comment" aria-label="delete comment"></i>
-        </button>
-      </div>
-    </div>
-  </div>
+  <p class="d-flex">
+    {{comment.body}}
+  </p>
+
 </template>
 
 
@@ -32,19 +18,9 @@ export default {
       required: true
     }
   },
-  setup(props) {
+  setup() {
     return {
       account: computed(() => AppState.account),
-      comment: computed(() => AppState.comments),
-      async removeComment() {
-        try {
-          const yes = await Pop.confirm('Delete Your Comment')
-          if (!yes) { return }
-          await commentsService.removeComment(props.comment.id)
-        } catch (error) {
-          Pop.error('[Remove Comment]', error)
-        }
-      }
     }
   }
 }
