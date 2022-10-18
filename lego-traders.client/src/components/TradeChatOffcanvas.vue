fd1080b463adb4cf9ef4f65d460dd54913317264
Coel Allen <coelallen@gmail.com>
2022-10-18 15:36:58 -0600

    coel and randy doin stuff

--- lego-traders.client/src/components/TradeChatOffcanvas.vue
+++ lego-traders.client/src/components/TradeChatOffcanvas.vue
@@ -2,16 +2,21 @@
   <div class="trade-chat-offcanvas">
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+        <h5 class="offcanvas-title" id="offcanvasRightLabel">Trade Chat</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body">
+      <div class="offcanvas-body d-flex">
         <!-- NOTE chat comments go here -->
         <ChatComment v-for="c in comments" :key="c.id" :comment="c" />
+        <form @submit.prevent="handleSubmit()">
+
+          <div class="input-group mb-3 align-self-end">
+            <input type="text" class="form-control" v-model="editable.body" placeholder="What you say?!"
+              aria-label="Recipient's username" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Button</button>
+          </div>
+        </form>
       </div>
-    </div>
-    <div class="offcanvas-footer">
-      Test!
     </div>
 
   </div>
@@ -20,18 +25,32 @@
 
 <script>
 import { computed } from '@vue/reactivity';
-import { onMounted } from 'vue';
+import { ref } from 'vue';
 import { AppState } from '../AppState.js';
+import { AuthService } from '../services/AuthService.js';
 import { commentsService } from '../services/CommentsService.js';
 import Pop from '../utils/Pop.js';
 import ChatComment from './ChatComment.vue';
 export default {
+  props: {
+    tradeId: { type: String }
+  },
   setup() {
-
+    const editable = ref({})
     return {
-      activeTrade: computed(() => AppState.activeTrade),
-      comments: computed(() => AppState.comments)
-
+      editable,
+      comments: computed(() => AppState.comments),
+      async handleSubmit() {
+        try {
+          if (!AppState.account.id) {
+            return AuthService.loginWithRedirect()
+          }
+          editable.value.tradeId = tradeId
+          await commentsService.createComment(editable.value)
+        } catch (error) {
+          Pop.error(error)
+        }
+      }
     };
   },
   components: { ChatComment }
