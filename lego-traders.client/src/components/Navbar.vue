7c31454b7c0bfd29ae6ede108466adf65b3c1e06
Randyhall91 <randyrocks11@gmail.com>
2022-10-19 14:26:16 -0600

    marketplace navbar refresh

--- lego-traders.client/src/components/Navbar.vue
+++ lego-traders.client/src/components/Navbar.vue
@@ -4,74 +4,47 @@
       <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
         <div class="d-flex gap-1 align-items-center">
           <img alt="logo" src="../assets/img/lego-logo.png" height="45" />
-          <img
-            class="img-fluid trader-img"
-            src="../assets/img/lego-trader.png"
-            alt="logo-text"
-          />
+          <img class="img-fluid trader-img" src="../assets/img/lego-trader.png" alt="logo-text" />
         </div>
       </router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarText"
-        aria-controls="navbarText"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto">
           <li>
             <router-link :to="{ name: 'Marketplace' }">
-              <button
-                class="marketplace text-shadow btn btn-danger selectable text-uppercase"
-              >
+              <button @click="getTradableSets()"
+                class="marketplace text-shadow btn btn-danger selectable text-uppercase">
                 Marketplace
               </button>
             </router-link>
           </li>
           <li>
             <router-link :to="{ name: 'Forum' }">
-              <button
-                class="forum text-shadow btn btn-primary text-uppercase text-light"
-              >
+              <button class="forum text-shadow btn btn-primary text-uppercase text-light">
                 Forum
               </button>
             </router-link>
           </li>
           <li>
-            <router-link
-              :to="{ name: 'MOC' }"
-              class="moc text-shadow btn btn-success selectable text-uppercase text-light"
-            >
+            <router-link :to="{ name: 'MOC' }"
+              class="moc text-shadow btn btn-success selectable text-uppercase text-light">
               Create Your Own
             </router-link>
           </li>
           <li>
-            <router-link
-              :to="{ name: 'Collection' }"
-              class="collections text-shadow btn btn-secondary selectable text-uppercase text-light"
-            >
+            <router-link :to="{ name: 'Collection' }"
+              class="collections text-shadow btn btn-secondary selectable text-uppercase text-light">
               Collections
             </router-link>
           </li>
         </ul>
         <!-- NOTIFICATIONS GO HERE-->
         <div class="dropdown">
-          <button
-            class="btn text-white me-3"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <i
-              class="fa-solid fa-bell fs-2 text-shadow"
-              title="Notifications"
-              aria-label="Notification tray"
-            ></i>
+          <button class="btn text-white me-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fa-solid fa-bell fs-2 text-shadow" title="Notifications" aria-label="Notification tray"></i>
           </button>
           <ul class="dropdown-menu dropdown-menu-end">
             <li v-for="n in state.notifications">
@@ -89,6 +62,7 @@
 <script>
 import { computed, reactive } from "@vue/reactivity";
 import { AppState } from "../AppState.js";
+import { legoSetsService } from "../services/LegoSetsService.js";
 import Login from "./Login.vue";
 import Notification from "./Notification.vue";
 export default {
@@ -96,7 +70,16 @@ export default {
     const state = reactive({
       notifications: computed(() => AppState.notifications),
     });
-    return { state };
+    return {
+      state,
+      async getTradableSets() {
+        try {
+          await legoSetsService.getTradableSets();
+        } catch (error) {
+          Pop.error(error, "[gettingTradableSets]");
+        }
+      }
+    };
   },
   components: { Login, Notification },
 };
