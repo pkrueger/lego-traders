b741230eccaa10027f6f2886d8d7676fd4182d50
EricTimRussell <erictim.russell@live.com>
2022-10-21 21:24:49 -0600

    set details more stuff

--- lego-traders.client/src/pages/SetDetailsPage.vue
+++ lego-traders.client/src/pages/SetDetailsPage.vue
@@ -2,52 +2,34 @@
   <!-- Set Details -->
   <div class="container-fluid px-4 bg-light">
     <div class="row py-3">
+      <router-link :to="{ name: 'Collection'}">
+        <i class="mdi mdi-arrow-left-thick fs-3" title="Collections Page"></i>
+      </router-link>
       <div class="col-12 d-flex gap-5 justify-content-center pb-3">
-        <button
-          class="btn btn-primary button-size"
-          v-if="!legoSet.ownerId"
-          @click="addSetToAccount(legoSet)"
-        >
+        <button class="btn btn-primary button-size" v-if="!legoSet.ownerId" @click="addSetToAccount(legoSet)">
           Add to Account
         </button>
-        <button
-          v-if="!legoSet.ownerId"
-          @click="addSetToWishList(legoSet)"
-          class="btn btn-primary button-size"
-          title="Add to Wishlist"
-        >
+        <button v-if="!legoSet.ownerId" @click="addSetToWishList(legoSet)" class="btn btn-primary button-size"
+          title="Add to Wishlist">
           Add to Wishlist
         </button>
-        <button
-          class="btn btn-primary button-size"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasRight"
-          aria-controls="offcanvasRight"
-        >
+        <button class="btn btn-primary button-size" type="button" data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
           Parts List
         </button>
-        <a
-          :href="
-            'https://www.google.com/search?tbm=shop&hl=en&psb=1&ved=0CAAQvOkFahcKEwiAkYjb1e_6AhUAAAAAHQAAAAAQEQ&q=' +
-            `lego set number ${legoSet.set_num} ${legoSet.name}`
-          "
-          target="_blank"
-          title="Search set on Google Shopping"
-        >
+        <a :href="
+          'https://www.google.com/search?tbm=shop&hl=en&psb=1&ved=0CAAQvOkFahcKEwiAkYjb1e_6AhUAAAAAHQAAAAAQEQ&q=' +
+          `lego set number ${legoSet.set_num} ${legoSet.name}`
+        " target="_blank" title="Search set on Google Shopping">
           <button class="btn btn-danger mdi mdi-shopping fs-5"></button>
         </a>
       </div>
+      <h4 class="text-center">{{ legoSet.name }}</h4>
       <div class="col-md-12 d-flex justify-content-center">
-        <img
-          :src="legoSet.set_img_url"
-          class="img-fluid set-img"
-          :title="legoSet.name"
-        />
+        <img :src="legoSet.set_img_url" class="img-fluid set-img" :title="legoSet.name" />
       </div>
       <div class="col-sm-12 col-md-12 p-1">
         <div class="text-center d-flex justify-content-center gap-5">
-          <h4>{{ legoSet.name }}</h4>
           <h4>Set Number {{ legoSet.set_num }}</h4>
           <h4>Realease Year: {{ legoSet.year }}</h4>
           <h4>{{ legoSet.num_parts }} pcs</h4>
@@ -57,17 +39,10 @@
     <!-- MOC sets -->
     <div class="row" v-if="mocSets.length">
       <h1>Alternate MOC's</h1>
-      <div
-        class="col-12 col-md-12 col-lg-3 d-flex justify-content-center p-3 border-test"
-        v-for="m in mocSets"
-      >
+      <div class="col-12 col-md-12 col-lg-3 d-flex justify-content-center p-3 border-test" v-for="m in mocSets">
         <div class="card moc-card text-center">
           <a :href="m.moc_url" target="_blank">
-            <img
-              :src="m.moc_img_url"
-              title="Rebrickable set details"
-              class="img-fluid moc-img pointer"
-            />
+            <img :src="m.moc_img_url" title="Rebrickable set details" class="img-fluid moc-img pointer" />
           </a>
           <div class="card-body text-dark">
             <h5>{{ m.name }} || {{ m.num_parts }} pcs</h5>
@@ -81,19 +56,11 @@
     <!-- Comments -->
     <div class="row">
       <div class="col-md-6 d-flex order-sm-1">
-        <form @submit.prevent="handleSubmit">
+        <form @submit.prevent="handleSubmit" v-if="account.id">
           <div class="px-1">
             <div class="input-group" style="height: 50px">
-              <input
-                type="text"
-                v-model="editable.body"
-                required
-                class="form-control"
-                placeholder="Comments..."
-                aria-label="comment body"
-                aria-describedby="button-addon1"
-                maxlength="250"
-              />
+              <input type="text" v-model="editable.body" required class="form-control" placeholder="Comments..."
+                aria-label="comment body" aria-describedby="button-addon1" maxlength="250" />
               <button class="btn btn-primary" type="submit" id="body">
                 Post
               </button>
@@ -106,15 +73,8 @@
         <div class="d-flex" v-if="owners.length > 0">
           <h3 class="px-5">Trade Requests</h3>
           <div v-for="o in owners">
-            <router-link
-              :to="{ name: 'Profile', params: { profileId: o.owner.id } }"
-            >
-              <img
-                :src="o.owner?.picture"
-                alt="user picture"
-                class="user-img"
-                :title="o.owner?.name"
-              />
+            <router-link :to="{ name: 'Profile', params: { profileId: o.owner.id } }">
+              <img :src="o.owner?.picture" alt="user picture" class="user-img" :title="o.owner?.name" />
             </router-link>
           </div>
         </div>
@@ -122,10 +82,7 @@
           <h3>No Trades</h3>
         </div>
       </div>
-      <div
-        class="col-sm-12 col-md-7 card bg-grey p-2 m-3 order-sm-3"
-        v-if="comments.length > 0"
-      >
+      <div class="col-sm-12 col-md-7 card bg-grey p-2 m-3 order-sm-3" v-if="comments.length > 0">
         <LegoSetComments v-for="c in comments" :key="c.id" :comment="c" />
       </div>
       <div v-else>
@@ -135,35 +92,17 @@
   </div>
 
   <!-- OffCanvas -->
-  <div
-    class="offcanvas offcanvas-end overflow2 container-fluid"
-    tabindex="-1"
-    id="offcanvasRight"
-    aria-labelledby="offcanvasRightLabel"
-    style="width: 50%"
-  >
+  <div class="offcanvas offcanvas-end overflow2 container-fluid" tabindex="-1" id="offcanvasRight"
+    aria-labelledby="offcanvasRightLabel" style="width: 50%">
     <div class="row">
       <div class="offcanvas-header bg-primary">
         <h5 class="offcanvas-title" id="offcanvasRightLabel">Parts List</h5>
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-          title="close"
-        ></button>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" title="close"></button>
       </div>
-      <div
-        class="offcanvas-body overflow bg-primary col-sm-12 col-md-6 col-lg-2"
-        v-for="s in setParts"
-      >
+      <div class="offcanvas-body overflow bg-primary col-sm-12 col-md-6 col-lg-2" v-for="s in setParts">
         <a :href="s.part.part_url" target="_blank">
           <div class="d-flex justify-content-center part-border">
-            <img
-              :src="s.part.part_img_url"
-              :title="s.part.name"
-              class="part-img"
-            />
+            <img :src="s.part.part_img_url" :title="s.part.name" class="part-img" />
           </div>
         </a>
       </div>
@@ -235,6 +174,7 @@ export default {
     }
     return {
       editable,
+      account: computed(() => AppState.account),
       legoSet: computed(() => AppState.activeApiSet),
       mocSets: computed(() => AppState.activeMOCset),
       setParts: computed(() => AppState.activeApiSetParts),
