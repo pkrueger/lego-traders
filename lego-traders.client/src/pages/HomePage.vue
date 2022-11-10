8cf43a4bf01a7ef52e0c42764f13db07139db773
Andrew Greenlaw <a.greenlaw8@gmail.com>
2022-11-10 16:04:28 -0700

    added images

--- lego-traders.client/src/pages/HomePage.vue
+++ lego-traders.client/src/pages/HomePage.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="container-fluid home bg-primary">
-    <div class="row justify-content-center">
-      <div class="col-10 my-5 text-center bg-light p-5">
-        <img src="../assets/img/welcome banner.png" alt="welcome banner">
-      </div>
-    </div>
     <div class="row">
-
+      <div class="col-12 p-4">
+        <img class="img-fluid profile elevation-1 m-4" src="ProfilePics\PatrickKrueger.jpg" alt="Patrick">
+        <img class="img-fluid profile elevation-1 m-4" src="" alt="Coel">
+        <img class="img-fluid profile elevation-1 m-4" src="ProfilePics\RandyHall.jpg" alt="Randy">
+        <img class="img-fluid profile elevation-1 m-4" src="" alt="Eric">
+        <img class="img-fluid profile elevation-1 m-4" src="ProfilePics\AndrewGreenlaw.jpg" alt="Andrew">
+      </div>
     </div>
     <!-- <div class="row">
       <div class="col-3 text-center">
@@ -49,7 +50,16 @@ export default {
 .home {
   min-height: 100%;
   background-image: url('https://images.unsplash.com/photo-1585366119957-e9730b6d0f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80');
-  background-position: bottom;
   background-size: cover;
+  background-position: bottom;
+}
+
+.profile {
+  height: 15rem;
+  width: 15rem;
+  object-fit: cover;
+  object-position: top;
+  border-radius: 50%;
+
 }
 </style>
