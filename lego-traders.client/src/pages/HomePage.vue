65f2b7d349975ea6a873f58091769f985fa82647
Coel Allen <coelallen@gmail.com>
2022-11-17 13:16:04 -0700

    coels picture and npm build

--- lego-traders.client/src/pages/HomePage.vue
+++ lego-traders.client/src/pages/HomePage.vue
@@ -2,23 +2,11 @@
   <div class="container-fluid home bg-primary">
     <div class="row">
       <div class="col-12 p-4">
-        <img
-          class="img-fluid profile elevation-1 m-4"
-          src="/ProfilePics/PatrickKrueger.jpg"
-          alt="Patrick"
-        />
-        <img class="img-fluid profile elevation-1 m-4" src="" alt="Coel" />
-        <img
-          class="img-fluid profile elevation-1 m-4"
-          src="/ProfilePics/RandyHall.jpg"
-          alt="Randy"
-        />
+        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/PatrickKrueger.jpg" alt="Patrick" />
+        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/CoelAllen.jpg" alt="Coel" />
+        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/RandyHall.jpg" alt="Randy" />
         <img class="img-fluid profile elevation-1 m-4" src="" alt="Eric" />
-        <img
-          class="img-fluid profile elevation-1 m-4"
-          src="/ProfilePics/AndrewGreenlaw.jpg"
-          alt="Andrew"
-        />
+        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/AndrewGreenlaw.jpg" alt="Andrew" />
       </div>
     </div>
   </div>
