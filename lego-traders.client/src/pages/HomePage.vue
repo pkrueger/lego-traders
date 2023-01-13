6413aec380e7a1f9eb691f8a25db25b9796c09ad
pkrueger <sendthistopatrick@gmail.com>
2023-01-13 13:32:13 -0700

    Home page changes

--- lego-traders.client/src/pages/HomePage.vue
+++ lego-traders.client/src/pages/HomePage.vue
@@ -1,12 +1,42 @@
 <template>
-  <div class="container-fluid home bg-primary">
+  <div class="container-fluid home bg-light">
     <div class="row">
+      <div class="col-12 text-center p-5">
+        <div class="welcome-banner bg-white p-4 rounded shadow">
+          <h1 class="welcome-text font--lego">
+            <span class="red">Welcome</span>
+            <span class="blue">to</span>
+            <span class="green">lego</span>
+            <span class="yellow">trader</span>
+          </h1>
+        </div>
+      </div>
       <div class="col-12 p-4">
-        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/PatrickKrueger.jpg" alt="Patrick" />
-        <img class="img-fluid profile-coel elevation-1 m-4" src="/ProfilePics/CoelAllen.jpg" alt="Coel" />
-        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/RandyHall.jpg" alt="Randy" />
-        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/Eric.jpg" alt="Eric" />
-        <img class="img-fluid profile elevation-1 m-4" src="/ProfilePics/AndrewGreenlaw.jpg" alt="Andrew" />
+        <img
+          class="img-fluid profile elevation-1 m-4"
+          src="/ProfilePics/PatrickKrueger.jpg"
+          alt="Patrick"
+        />
+        <img
+          class="img-fluid profile-coel elevation-1 m-4"
+          src="/ProfilePics/CoelAllen.jpg"
+          alt="Coel"
+        />
+        <img
+          class="img-fluid profile elevation-1 m-4"
+          src="/ProfilePics/RandyHall.jpg"
+          alt="Randy"
+        />
+        <img
+          class="img-fluid profile elevation-1 m-4"
+          src="/ProfilePics/Eric.jpg"
+          alt="Eric"
+        />
+        <img
+          class="img-fluid profile elevation-1 m-4"
+          src="/ProfilePics/AndrewGreenlaw.jpg"
+          alt="Andrew"
+        />
       </div>
     </div>
   </div>
@@ -24,9 +54,33 @@ export default {
 <style scoped lang="scss">
 .home {
   min-height: 100%;
-  background-image: url("https://images.unsplash.com/photo-1585366119957-e9730b6d0f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80");
-  background-size: cover;
-  background-position: bottom;
+}
+
+.welcome-banner {
+}
+
+.welcome-text {
+  font-size: 8rem;
+
+  span {
+    margin-right: 2.5rem;
+  }
+
+  .red {
+    color: #da291c;
+  }
+
+  .blue {
+    color: #0055bf;
+  }
+
+  .green {
+    color: #4b9f4a;
+  }
+
+  .yellow {
+    color: #ffd700;
+  }
 }
 
 .profile {
